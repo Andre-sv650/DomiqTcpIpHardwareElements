4f9b811e6b2c0210a6442c352c90ecef6bddd8c9
Andre-sv650 <41384845+Andre-sv650@users.noreply.github.com>
2019-03-25 21:44:05 +0100

    Add files via upload

---
+++ src/connected_hardware_elements/keypad/keypad_4_4.cpp
@@ -0,0 +1,43 @@
+
+#include "keypad_4_4.h"
+#ifdef KEYPAD_4_4_ELEMENT_ENABLED
+
+
+const Uint8 ROWS = 4; //four rows
+const Uint8 COLS = 4; //four columns
+
+//define the symbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'0','1','2','3'},
+  {'4','5','6','7'},
+  {'8','9','A','B'},
+  {'C','D','E','F'}
+};
+
+/* Initialize the 4 * 4 keyboard.
+   StartPinForRow The row pin is 4 width. that means that 4 pins are needed from the start pin.
+   StartPinForColumn The column pin is 4 width, that means that 4 pins are needed from the start pin. For example start with pin 0.
+   VarElementNameInDomiq The element name in domiq base. For example start with 4.
+*/
+void KEYPAD_4_4_ELEMENT::initiate(Uint8 StartPinForRow, Uint8 StartPinForColumn, const String &VarElementNameInDomiq)
+{
+    Uint8 rowPins[ROWS] = {StartPinForRow + 3u, StartPinForRow + 2u, StartPinForRow + 1u, StartPinForRow}; //connect to the row pinouts of the keypad
+    Uint8 colPins[COLS] = {StartPinForColumn + 3u, StartPinForColumn + 2u, StartPinForColumn + 1u, StartPinForColumn}; //connect to the column pinouts of the keypad
+    
+    Pad.initialize(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+    CONNECTED_ELEMENT_BASE::initiate(VarNameInDomiq);
+
+}
+
+
+void KEYPAD_4_4_ELEMENT::background_routine(void)
+{
+    char key = Pad.getKey();
+    String keyValue = String(key);
+
+    CONNECTED_ELEMENT_BASE::set_new_data_sampled(keyValue);
+}
+
+
+#endif // KEYPAD_4_4_ELEMENT_ENABLED
---
+++ src/connected_hardware_elements/keypad/keypad_4_4.h
@@ -0,0 +1,29 @@
+
+
+#include "../../AllYouNeedIsStartHere/project_defines.h"
+#ifdef KEYPAD_4_4_ELEMENT_ENABLED
+
+#ifndef KEYPAD_4_4_ELEMENT_H
+#define KEYPAD_4_4_ELEMENT_H
+
+#include "../connected_element_base.h"
+#include "../../external_libraries/keypad.h"
+/*
+ * One keypad 4 * 4 element
+ */
+class KEYPAD_4_4_ELEMENT : public CONNECTED_ELEMENT_BASE
+{
+    public:
+
+    void initiate(Uint8 StartPinForRow, Uint8 StartPinForColumn, const String &VarElementNameInDomiq);
+
+
+    void background_routine(void);
+
+    private:
+    Keypad Pad;
+};
+
+#endif //KEYPAD_4_4_ELEMENT_H
+
+#endif //KEYPAD_4_4_ELEMENT_ENABLED
