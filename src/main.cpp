90eac388903ae8988fcad02522a23a95419153e3
Andre-sv650 <41384845+Andre-sv650@users.noreply.github.com>
2019-05-05 13:41:32 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
-
+#include "AllYouNeedIsStartHere/project_defines.h"
 #include <SPI.h>
 #include "AllYouNeedIsStartHere/sensor_elements_initiate.h"
+#include "AllYouNeedIsStartHere/sensor_elements_keller_initiate.h"
 #include "tcp_server/tcp_server.h"
-#include "connected_hardware_elements/connected_element_array.h"
+#include "connected_hardware_elements/Base/connected_element_array.h"
+#include "HelperFunctions/eeprom_initialize.h"
  
-// Initialize the Ethernet client library
+
+#ifdef KELLER
+SENSOR_ELEMENTS_KELLER_INITIATE sensorElements;
+#else
 SENSOR_ELEMENTS_INITIATE sensorElements;
+#endif
+
+// Initialize the Ethernet client library
 TCP_SERVER server;
  
 void setup()
 {
- // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
 
+  //Delay of 500ms to ensure that the connected PC is ready for serial communication.
+  delay(500)
+
+  //Check if the EEPROM has to be initialized.
+  EEPROM_INITIALIZE::initialize(EEPROM_KEY1, EEPROM_KEY2);
+
+  //Initialize the sensor elements.
   sensorElements.initiate();
 
   //Try to establish connection to base till it is ready.
