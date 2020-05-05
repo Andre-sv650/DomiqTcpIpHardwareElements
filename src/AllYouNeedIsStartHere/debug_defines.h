3f1392ef40c2174ded334d4639fa213113436b95
Andre-sv650 <41384845+Andre-sv650@users.noreply.github.com>
2020-05-05 13:57:34 +0200

    Added the RC switch functionality

--- src/AllYouNeedIsStartHere/debug_defines.h
+++ src/AllYouNeedIsStartHere/debug_defines.h
@@ -2,29 +2,39 @@
 #define DEBUG_DEFINES_H
 
 //Section TCP server
-//#define DEBUG_DATA_TCP_SERVER
-#define DEBUG_DATA_SENSOR_ELEMENTS_ALL
+//#define DEBUG_DATA_TCP_SERVER //Reports all incoming messages from the TCP/IP interface that are received from or send to domiq.
+#define DEBUG_DATA_SENSOR_ELEMENTS_ALL //Reports incoming and outgoing data from the sensors that are connected.
+
+//Section: Base debug define. This enables all messages that are outgoing to domiq base.
+//Enable the switch for debugging. Disable it to be faster and use less time for sending data to the console of visual studio code.
+//This is making a hughe difference in performance. So only use it if something is not working how it is expected.
+//#define DEBUG_CONNECTED_ELEMENT_BASE_MESSAGES_TO_DOMIQ
+//#define DEBUG_CONNECTED_ELEMENT_BASE_MESSAGES_FROM_DOMIQ
 
 //Section Connected element array
 //#define DEBUG_CONNECTED_ELEMENT_ARRAY
 
-
 //Section Relay
-//#define DEBUG_RELAIS_SET
-//#define DEBUG_RELAIS_GET
+#define DEBUG_RELAIS_SET
+#define DEBUG_RELAIS_GET
 
+//Section DHT22 sensor
+//#define DEBUG_DHT22
 
-
-//Section DHT22
-#define DEBUG_DHT22
-
-//Section One wire
-#define DEBUG_TEMP_SENSOR_ONE_WIRE
+//Section One wire sensor
+//#define DEBUG_TEMP_SENSOR_ONE_WIRE
 
 //Section light intensity element
-#define DEBUG_LIGHT_INTENSITY_ELEMENT
+//#define DEBUG_LIGHT_INTENSITY_ELEMENT
 
 //Section presence detector element
-#define DEBUG_PRESENCE_DETECTOR_ELEMENT
+//#define DEBUG_PRESENCE_DETECTOR_ELEMENT
+
+//Debug for the interrupt element
+//#define DEBUG_INTERRUPT_ELEMENT
+
+//The debug for the RC switch element.
+#define DEBUG_RC_SWITCH_ELEMENT
+#define DEBUG_RC_SENDER_ELEMENT
 
 #endif /* MODBUS_DEBUG_DEFINES_H */
--- src/AllYouNeedIsStartHere/project_defines.h
+++ src/AllYouNeedIsStartHere/project_defines.h
@@ -1,24 +1,24 @@
-#define KELLER
-
 //---------------------Setup a new key if the EEPROM should be erased.--------------------
 #define EEPROM_KEY1 1
 #define EEPROM_KEY2 2
+
+//The communication speed with linux or windows.
+#define SERIAL_COMMUNICATION_SPEED 115200
 
 //Save the data to the EEPROM every minute. Else the eeprom is destroyed after small time by too many write attmepts.
 #define INTERRUPT_ELEMENT_SAVE_DATA_TO_EEPROM_TIME 60000
 
 
 //---------------The maximum number of connected elements like DHT22, temp seonsor elements one wire etc. -------------
-#define CONNECTED_ELEMENT_ARRAY_ELEMENTS_LENGTH 6
-
-
+#define CONNECTED_ELEMENT_ARRAY_ELEMENTS_LENGTH 10
 
 //-------------------------------Defines for the enabled elements.-------------------------------------
 #define RELAIS_ELEMENT_ENABLED //Define it to use relay elements.
+#define RELAIS_ELEMENT_MAX_NUMBER_OF_RELAIS 8 //The maximum number of relais per relais element.
 
 //The relais element self locked switch on and off time in milli seconds.
-//Normally a relais dont need more than 80ms to be switched on and off. We use the double time.
-#define RELAIS_ELEMENT_SELF_LOCKED_RELAIS_ON_OFF_TIME 160
+//Normally a relais dont need more than 80ms to be switched on and off. We use 250ms to be on the safe side.
+#define RELAIS_ELEMENT_SELF_LOCKED_RELAIS_ON_OFF_TIME 250
 
 
 #define DHT22_ELEMENT_ENABLED //Define it to used DHT22 elements
@@ -30,11 +30,14 @@
 
 
 //#define STM_VL53L0X_ELEMENT_ENABLED //Define it to use STM VL53LOX range sensor.
-//#define KEYPAD_4_4_ELEMENT_ENABLED //Define it to use the 4_4 keypad element.
+#define KEYPAD_4_4_ELEMENT_ENABLED //Define it to use the 4_4 keypad element.
+
+#define RC_SWITCH_433MHZ_ENABLED //Define it to use the RC switch (incoming data from RC control)
+#define RC_SENDER_433MHZ_ENABLED //Define it to use the RC sender. Enable or disble external components like external power plugs.
 
 
 //#define PRESENCE_DETECTOR_ELEMENT_ENABLED
 
 
-#define MaximumNumberOfInterrupts 2
+#define MaximumNumberOfInterrupts 10
 #define INTERRUPT_ELEMENT_ENABLED
