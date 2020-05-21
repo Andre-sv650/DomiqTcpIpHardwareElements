d5a868cfa68cd4a26032517eaa9d47fee3aa8ebf
Andre-sv650 <41384845+Andre-sv650@users.noreply.github.com>
2020-05-21 23:36:14 +0200

    String copy not needed any more.

--- src/debug/debug_data.cpp
+++ src/debug/debug_data.cpp
@@ -8,27 +8,30 @@ namespace DEBUG_DATA
   {
     #ifdef DEBUG_DATA_TCP_SERVER
     
-    Serial.println("Failed to configure Ethernet using DHCP");
+    Serial.println(F("Failed: DHCP"));
 
     #endif
   }
 
-  void tcp_server_connected_to_network(String IpAddress)
+  void tcp_server_connected_to_network(IPAddress IpAddress)
   {
     #ifdef DEBUG_DATA_TCP_SERVER
-    
-    Serial.println("Own IP address is: " + IpAddress);
+        
+    Serial.print(F("Own IP address: "));
+    Serial.println(IpAddress);
 
     #endif
   }
   
   
-  void tcp_server_print_command_received(Uint8 Length, const String &DataValue)
+  void tcp_server_print_command_received(const String &DataValue)
   {
     #ifdef DEBUG_DATA_TCP_SERVER
     
-    String serialPrintfData = "New data from domiq received. Length: " + String(Length) + ". Data is: " + DataValue;
-    Serial.println(serialPrintfData);
+    Serial.print(F("Data received. Length: "));
+    Serial.print(String(DataValue.length()));
+    Serial.print(F(". Data: "));
+    Serial.print(DataValue);
 
     #endif
   }
@@ -38,54 +41,58 @@ namespace DEBUG_DATA
   {
     #ifdef DEBUG_DATA_TCP_SERVER
 
-    String printLine = "Sending data to domiq done, data was: " + Data;
-    Serial.println(printLine); 
+    Serial.print(F("Send data done, data: "));
+    Serial.print(Data); 
 
     #endif
   }
 
 
-  void connected_element_array_item_added(String ElementName)
+  void connected_element_array_item_added(String &ElementName)
   {
-    Serial.println("A new element was registered: " + ElementName);
+    Serial.print(F("Element registered: "));
+    Serial.println(ElementName);
   }
 
 
   void connected_element_array_item_found_setting_data(String &ItemName, String &Value)
   {
     #ifdef DEBUG_CONNECTED_ELEMENT_ARRAY
-    Serial.println("Element found, setting data for: " + ItemName + ". With Value: " + Value);
+    Serial.print(F("Element found, set data: "));
+    Serial.println(ItemName + ". Value: " + Value);
 
     #endif
   }
 
 
-  void connected_element_array_found_new_data(String &VariableName, String &Value)
+  void connected_element_array_found_new_data(String &Value)
   {
     #ifdef DEBUG_CONNECTED_ELEMENT_ARRAY
-    Serial.println("New data was found for variale: " + VariableName + ". With value " + Value);
+    Serial.print(F("New data for variable: "));
+    Serial.println(Value);
 
     #endif
   }
 
 
 
-  void relais_element_set_data_called(String NewData)
+  void relais_element_set_data_called(String &NewData)
   {
     #ifdef DEBUG_RELAIS_SET
 
-    String serialPrintData = "New data from domiq for Relay was received. Data: " + NewData;
-    Serial.println(serialPrintData);
+    Serial.print(F("Data for Relay received: "));
+    Serial.println(NewData);
 
     #endif
   }
 
-  void relais_element_set_data_to_pin(char Pin)
+  void relais_element_set_data_to_pin(Uint8 Pin, Uint8 Value)
   {
     #ifdef DEBUG_RELAIS_SET
-    
-    String serialPrintData = "Setting Pin: " + Pin;
-    Serial.println(serialPrintData);
+    Serial.print(F("Setting Pin: "));
+    Serial.print(Pin);
+    Serial.print(F(". To value: "));
+    Serial.println(Value);
 
     #endif
   }
@@ -93,23 +100,16 @@ namespace DEBUG_DATA
   void relais_element_get_data_called(String &ReturnedValue)
   {
     #ifdef DEBUG_RELAIS_SET
-    
-    String serialPrintData = "Get data was called, returned the follwing data: " + ReturnedValue;
-    Serial.println(serialPrintData);
+    Serial.print(F("Get data called, returned data: "));
+    Serial.println(ReturnedValue);
 
     #endif
   }
 
   void temp_sensor_one_wire_new_data_sampled(String &NewData)
   {
-    String tempSensorData = "New temp sensor data sampled: " + NewData;
-    Serial.println(tempSensorData);
-  }
-
-
-  void generic_send_debug_message(String &Message)
-  {
-    Serial.println(Message);
+    Serial.print(F("New temp sensor one wire sampled: "));
+    Serial.println(NewData);
   }
 }
 
--- src/debug/debug_data.h
+++ src/debug/debug_data.h
@@ -2,37 +2,36 @@
 #include "../AllYouNeedIsStartHere/project_defines.h"
 #include "../AllYouNeedIsStartHere/debug_defines.h"
 #include "../datatypes/cpu_datatypes.h"
+#include "IPAddress.h"
 #include <WString.h>
 
 namespace DEBUG_DATA
 {
   void tcp_server_failed_to_connect(void);
 
-  void tcp_server_connected_to_network(String IpAddress);
+  void tcp_server_connected_to_network(IPAddress IpAddress);
   
-  void tcp_server_print_command_received(Uint8 Length, const String &DataValue);
+  void tcp_server_print_command_received(const String &DataValue);
 
 
   void tcp_server_print_send_new_data(String &Data);
 
 
 
-  void connected_element_array_item_added(String ElementName);
+  void connected_element_array_item_added(String &ElementName);
 
   void connected_element_array_item_found_setting_data(String &ItemName, String &Value);
 
-  void connected_element_array_found_new_data(String &VariableName, String &Value);
+  void connected_element_array_found_new_data(String &Value);
 
 
 
-  void relais_element_set_data_called(String NewData);
+  void relais_element_set_data_called(String &NewData);
 
-  void relais_element_set_data_to_pin(char Pin);
+  void relais_element_set_data_to_pin(Uint8 Pin, Uint8 Value);
 
   void relais_element_get_data_called(String &ReturnedValue);
 
 
   void temp_sensor_one_wire_new_data_sampled(String &NewData);
-
-  void generic_send_debug_message(String &Message);
 };
