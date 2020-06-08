36cfb455622332eeb9603a24e30eef913bb71c1f
Andre-sv650 <41384845+Andre-sv650@users.noreply.github.com>
2020-06-08 09:44:50 +0200

    Removed compile errors.

--- src/AllYouNeedIsStartHere/sensor_elements_initiate.cpp
+++ src/AllYouNeedIsStartHere/sensor_elements_initiate.cpp
@@ -1,4 +1,4 @@
-
+#ifdef ARDUINO_MEGA
 #include "sensor_elements_initiate.h"
 
 /*
@@ -10,17 +10,27 @@ SENSOR_ELEMENTS_INITIATE::SENSOR_ELEMENTS_INITIATE()
 
 void SENSOR_ELEMENTS_INITIATE::initiate(void)
 {
-    //One self locked relais for the 230V gartenpumpe.
+  //One self locked relais for the 230V gartenpumpe.
   //I used one relais of type.
-  SelfLockedRelais.initiate("VAR.Electrical24V", 22u, TRUE);
+  Electrical24VPowerSupply.initiate("VAR.Electrical24V", 22u, TRUE);
+
+  PowerSupplyPumpeAndBuchsbaumBeleuchtung.initiate("VAR.BuchsbaumAndPump230V", 23u, TRUE);
+
+  //Initialize the RC switch.
+  //RcSwitchElement.initiate("VAR.RcSwitch", 2u);
 
   //Initialize the relais for garten pumpe.
   RelaisForGartenpumpe.initiate("VAR.GartenPumpe", 24u, 1, TRUE);
   RelaisForGartenpumpe.set_maximum_on_time(MAXIMUM_TIME_FOR_RELAIS);
   
+
   //Initialize the relais for bewaesserung
   RelaisForGartenSprenger.initiate("VAR.Bewaesserung", 30u, 6u, TRUE, TRUE);
   RelaisForGartenSprenger.set_maximum_on_time(MAXIMUM_TIME_FOR_RELAIS);
+
+  //The external rc switch should turn the gartensprenger on and off.
+  //Uint32 RcSwitchCodesForRelais[8] = {5571921, 5571924, 5574993, 5574996, 5575761, 0, 0, 0};
+  //RelaisForGartenSprenger.connect_to_internal_rc_switch(&RcSwitchElement.LastReceivedCode, RcSwitchCodesForRelais, RcSwitchCodesForRelais);
 
   //Initialize the DHT22 sensor.
   //DHT22 Sensor needs a pull up resisor between SCL and VCC.
@@ -29,21 +39,31 @@ void SENSOR_ELEMENTS_INITIATE::initiate(void)
   //A one wire temp sensor on arduino pin 53.
   TempOneWireSensor.initiate("VAR.TempSensorOneWire", 42u);
 
+
+  //Initialize a S0 element for measurement of the solar collector.
+  PhotovoltaicPlantS0Element.initiate("VAR.PhotovoltaicPlant", 2u, INTERRUPT_ELEMENT_DEFAULT_ENTPRELL_TIME_FOR_FAST_IRQS, TRUE);
+
+  //Initialize the element for the rain measurement.
+  RainMeasurementElement.initiate("VAR.RegenMenge", 3u, INTERRUPT_ELEMENT_DEFAULT_ENTPRELL_TIME_FOR_SLOW_IRQS, TRUE);
+
+
   //Ligth intensity element BH1750. Connect VCC with 5V, GND with GND.
   //On arduino uno connect SCL with PIN A5 and SDA with PIN A4.
   //On arduino mega connect SCL with Pin Pin 21 and SDA with PIN 20.
   //See https://www.arduino.cc/en/Reference/Wire for details.
-  LightIntensitySensor.initiate("VAR.AussenHelligkeit");
+  LightIntensitySensor.initiate(20u, "VAR.AussenHelligkeit");
 
   //The STM VL53 range sensor.
   //StmVl53Element.initiate("VAR.StmRangeInMm");
 
-  TankFuellStandLinks.initiate("VAR.TankFuellstandLinks", 44u, 45u);
-
-  TankFuellStandRechts.initiate("VAR.TankFuellstandRechts", 46u, 47u);
+  //Initialize the ultrasonic elements for the drainage.
+  Drainage1.initiate("VAR.DrainageWasserstand1", 44u, 45u);
+  Drainage2.initiate("VAR.DrainageWasserstand2", 46u, 47u);
 
   //KeyPad.initiate(4u, 8u, "VAR.KeyPad");
 
   //All elements registered.
   //Serial.println("All elements registered. Starting server");
 }
+
+#endif //ARDUINO_MEGA
--- src/AllYouNeedIsStartHere/sensor_elements_initiate.h
+++ src/AllYouNeedIsStartHere/sensor_elements_initiate.h
@@ -1,4 +1,4 @@
-
+#ifdef ARDUINO_MEGA
 
 #include "datatypes/cpu_datatypes.h"
 #include "connected_hardware_elements/DHT22/dht22_element.h"
@@ -10,13 +10,14 @@
 #include "connected_hardware_elements/keypad/keypad_4_4.h"
 #include "connected_hardware_elements/ultrasonic/ultrasonic_sr04t_element.h"
 #include "connected_hardware_elements/interrupt/interrupt_element.h"
+#include "connected_hardware_elements/RcSwitch433Mhz/rc_switch_433_mhz.h"
 
 #ifndef SENSOR_ELEMENTS_INITIATE_H
 #define SENSOR_ELEMENTS_INITIATE_H
 
 //The maximum on time for the relais is set to 10 minutes = 60000ms.
 //This is needed in case that the connection to domiq is broken and you are on holidays.
-#define MAXIMUM_TIME_FOR_RELAIS 600000
+#define MAXIMUM_TIME_FOR_RELAIS 600000u
 
 class SENSOR_ELEMENTS_INITIATE
 {
@@ -33,7 +34,9 @@ public:
 
   RELAIS_ELEMENT RelaisForGartenSprenger;
 
-  RELAIS_ELEMENT_SELF_LOCKED SelfLockedRelais;
+  RELAIS_ELEMENT_SELF_LOCKED Electrical24VPowerSupply;
+
+  RELAIS_ELEMENT_SELF_LOCKED PowerSupplyPumpeAndBuchsbaumBeleuchtung;
 
   DHT22_ELEMENT Dth22Sensor;
 
@@ -43,17 +46,22 @@ public:
 
   //STM_VL53L0X_ELEMENT StmVl53Element;
 
-  ULTRASONIC_SR04T_ELEMENT TankFuellStandLinks;
+  ULTRASONIC_SR04T_ELEMENT Drainage1;
 
-  ULTRASONIC_SR04T_ELEMENT TankFuellStandRechts;
+  ULTRASONIC_SR04T_ELEMENT Drainage2;
 
-  INTERRUPT_ELEMENT S0CounterForDrainage1;
+  INTERRUPT_ELEMENT PhotovoltaicPlantS0Element;
+
+  INTERRUPT_ELEMENT RainMeasurementElement;
+
+  RC_SWITCH_433MHZ_ELEMENT RcSwitchElement;
 
   //KEYPAD_4_4_ELEMENT KeyPad;
   
 private:
 };
 
-
 #endif //SENSOR_ELEMENTS_INITIATE_H
+
+#endif //ARDUINO_MEGA
 
