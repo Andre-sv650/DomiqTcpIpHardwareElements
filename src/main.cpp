5b8f04661a9ef84d59d6474e5a92b50618bb8550
Andre-sv650 <41384845+Andre-sv650@users.noreply.github.com>
2020-06-18 09:51:40 +0200

    Added watchdog functionality

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "connected_hardware_elements/Base/connected_element_array.h"
 #include "connected_hardware_elements/RcSwitch433Mhz/rc_switch_433_mhz_base_instance.h"
 #include "HelperFunctions/eeprom_initialize.h"
+#include "watchdog/watchdog.h"
  
 
 #ifdef NODEMCU
@@ -44,7 +45,6 @@ void setup()
   Serial.begin(SERIAL_COMMUNICATION_SPEED);
 
   #ifdef NODEMCU
-
   //Node mcu does not have an EEPROM. EEPROM is not supported until now.
   #else
 
@@ -53,11 +53,25 @@ void setup()
 
   #endif
 
+  Serial.println(F("Software started"));
+
   //Initialize the sensor elements.
   sensorElements.initiate();
 
+  //Initiate the server.
+  server.initiate();
+
   //Try to establish connection to base till it is ready.
-  while(server.initiate() == FALSE);
+  while(server.connect_to_domiq() == FALSE){
+    //No connection available try again.
+    Serial.println(F("Connection failed, retry"));
+
+    //1000ms delay for the reconnection.
+    delay(1000);
+  }
+
+  //Start the software watchdog.
+  watchdog_enable();
 }
  
 void loop()
@@ -65,7 +79,12 @@ void loop()
   String sendData;
 
   //Check for incoming messages from Domiq base.
-  server.loop();
+  bool8 connectionOk = server.loop();
+
+  if(connectionOk == FALSE){
+    //The connection was reconnected. Report the last sampled data again.
+    CONNECTED_ELEMENT_ARRAY::report_sampled_data_to_domiq_after_reconnect();
+  }
 
   //Set the new data that was received.
   CONNECTED_ELEMENT_ARRAY::set_new_data_from_domiq(server.ReceivedData);
@@ -82,6 +101,9 @@ void loop()
   CONNECTED_ELEMENT_ARRAY::get_new_data(sendData);
 
   server.send_data(sendData);
+
+  //Reset the software watchdog.
+  watchdog_reset_time();
 }
 
 
